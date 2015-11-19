89ffe4bce1035093cb5de0c60f3e6d49fb13299b
Eric Becker <121onto@gmail.com>
2015-11-19 02:19:39 -0800

    Added utils for exploring local binary patterns.

--- noaa/facial_alignment.py
+++ noaa/facial_alignment.py
@@ -9,21 +9,47 @@ from random import shuffle
 import matplotlib.pyplot as plt
 from skimage.segmentation import slic
 from skimage.filters import threshold_yen
+import skimage.transform
+import skimage.feature
 from PIL import Image, ImageDraw
 from scipy import ndimage
 from scipy.ndimage import binary_dilation
 from scipy.ndimage import binary_opening
-
+from scipy.spatial import ConvexHull
 
 ###########################################################################
 ## local imports
 
 from noaa.utils import (lookup_whale_images, load_images,
-                   plot_images, plot_whales_by_id)
+                        plot_images, plot_images_and_histograms,
+                        plot_whales_by_id)
 from config import BASE_DIR
 
 ###########################################################################
 ## transforms
+
+def add_image_to_image_generator(images, file=None):
+    if file is not None:
+        path = os.path.join(BASE_DIR, 'data/imgs/')
+        path = os.path.join(path, file)
+        image = load_images(file).astype('int32')
+        images = itertools.chain([(path, image)], images)
+
+    return images
+
+
+def calculate_binary_opening_structure(binary_image):
+    s = 1 + 10000 * (binary_image.sum()/binary_image.size) ** 1.4
+    s = max(5, 3 * np.log(s))
+    return np.ones((s, s))
+
+
+def binary2gray(binary_image):
+    gray_image = np.zeros(binary_image.shape + (3,))
+    for i in range(3):
+        gray_image[binary_image > 0, i] = binary_image[binary_image > 0]
+    return gray_image
+
 
 def mask_polygon(verts, shape):
     img = Image.new('L', shape, 0)
@@ -34,7 +60,6 @@ def mask_polygon(verts, shape):
 
 
 def convex_hull_mask(data, mask=True):
-    from scipy.spatial import ConvexHull
     segm = np.argwhere(data)
     hull = ConvexHull(segm)
     verts = [(segm[v,0], segm[v,1]) for v in hull.vertices]
@@ -100,17 +125,63 @@ def smallest_partition(data, channel):
     return rtn
 
 
-from skimage.restoration import denoise_tv_chambolle, denoise_bilateral
+def mask_largest_regions(mask, num_regions=2):
+    rtn = np.copy(mask)
+    regions, n_labels = ndimage.label(mask)
+    label_list = range(1, n_labels+1)
+    sizes = []
+    for label in label_list:
+        size = (regions==label).sum()
+        sizes.append((size, label))
+
+    sizes = sorted(sizes, reverse=True)
+    num_regions = min(num_regions, n_labels-1)
+    min_size = sizes[num_regions][0]
+
+    remove = np.ones(regions.shape)
+    for size, label in sizes:
+        if size < min_size:
+            remove[regions==label] = 0
+
+    rtn[remove==1] = 0
+    return rtn
+
+
+def extract_largest_regions(mask, num_regions=2):
+    rtn = np.copy(mask)
+    regions, n_labels = ndimage.label(mask)
+    label_list = range(1, n_labels+1)
+    sizes = []
+    for label in label_list:
+        size = (regions==label).sum()
+        sizes.append((size, label))
+
+    sizes = sorted(sizes, reverse=True)
+    num_regions = min(num_regions, n_labels-1)
+    min_size = sizes[num_regions][0]
+
+    labels = []
+    for size, label in sizes:
+        if size < min_size:
+            regions[regions==label] = 0
+        else:
+            labels.append(label)
+
+    return regions, labels
+
+
 def denoise_image(data, type=None):
+    from skimage.restoration import denoise_tv_chambolle, denoise_bilateral
     if type == 'tv':
         return denoise_tv_chambolle(data, weight=0.2, multichannel=True)
 
     return denoise_bilateral(data, sigma_range=0.1, sigma_spatial=15)
 
 
-from skimage.feature import blob_dog, blob_log, blob_doh
-from skimage.color import rgb2gray
+
 def detect_blobs(data_gray):
+    from skimage.feature import blob_dog, blob_log, blob_doh
+    from skimage.color import rgb2gray
     # takes grayscale
     blobs = blob_doh(data_gray, min_sigma=10, num_sigma=3, max_sigma=50, threshold=.1)
     mask = np.zeros(data_gray)
@@ -122,7 +193,6 @@ def detect_blobs(data_gray):
     return mask
 
 
-from skimage.segmentation import slic
 def segment_image_slic(image, retain=None, n_segments=20, compactness=20, sigma=5):
         segments = slic(image, n_segments=n_segments, compactness=compactness, sigma=sigma)
         if retain is not None:
@@ -134,9 +204,6 @@ def segment_image_slic(image, retain=None, n_segments=20, compactness=20, sigma=
         return segments
 
 
-###########################################################################
-## feature extractions
-
 def segmentation(retain=3, n_segments=20, compactness=20, sigma=0):
     images = image_generator()
 
@@ -144,6 +211,70 @@ def segmentation(retain=3, n_segments=20, compactness=20, sigma=0):
         retained_segments = segment_image_slic(image, retain=retain, n_segments=n_segments, compactness=compactness, sigma=sigma)
         plot_images([image, retained_segments], ['Original', 'Segmented'])
 
+
+
+def threshold_by_channel(image, threshold, n_channels=3):
+    rtn = []
+    for i in xrange(n_channels):
+        ch = np.zeros(image.shape)
+        ch[image[:,:,i] > threshold, :] = 255
+        ch = smallest_partition(ch, i)
+        rtn.append(ch)
+
+    return rtn
+
+
+def minimum_bounding_rectangle(verts, return_rotation_matrix=False, return_rotation_angle=False):
+    pi2 = np.pi/2.
+
+    # compute rotation angles
+    verts = np.array(verts)
+    angles = np.empty(verts.shape[0])
+    angles = np.arctan2(verts[:, 1], verts[:, 0])
+
+    # compute rotations
+    rotations = np.vstack([
+        np.cos(angles),
+        np.cos(angles-pi2),
+        np.cos(angles+pi2),
+        np.cos(angles)
+    ]).T
+    rotations = rotations.reshape((-1, 2, 2))
+
+    #apply rotations to the verts
+    rot_points = np.dot(rotations, verts.T)
+
+    # find the bounding points
+    min_x = np.nanmin(rot_points[:, 0], axis=1)
+    max_x = np.nanmax(rot_points[:, 0], axis=1)
+    min_y = np.nanmin(rot_points[:, 1], axis=1)
+    max_y = np.nanmax(rot_points[:, 1], axis=1)
+
+    # find the smallest area
+    areas = (max_x - min_x) * (max_y - min_y)
+    best_idx = np.argmin(areas)
+
+    # extract the best points
+    x1 = max_x[best_idx]
+    x2 = min_x[best_idx]
+    y1 = max_y[best_idx]
+    y2 = min_y[best_idx]
+    r = rotations[best_idx]
+    angle = angles[best_idx]
+
+    rtn = np.zeros((4,2))
+    rtn[0,:] = np.dot([x1, y2], r)
+    rtn[1,:] = np.dot([x2, y2], r)
+    rtn[2,:] = np.dot([x2, y1], r)
+    rtn[3,:] = np.dot([x1, y1], r)
+
+    r = np.linalg.inv(r)
+    angle = math.atan2(r[1,0], r[0,0])
+    return rtn, r, angle
+
+
+###########################################################################
+## examine feature
 
 def thresholding(votes_min=3):
     from skimage.filters import threshold_otsu
@@ -225,95 +356,56 @@ def thresholding(votes_min=3):
         print('plotting')
         plot_images(
             thresholded_images,
-            titles,
+            titles=titles,
             suptitle=fn.split('/')[-1]
         )
 
 
-def threshold_by_channel(image, threshold, n_channels=3):
-    rtn = []
-    for i in xrange(n_channels):
-        ch = np.zeros(image.shape)
-        ch[image[:,:,i] > threshold, :] = 255
-        ch = smallest_partition(ch, i)
-        rtn.append(ch)
+def inspect_glare_patterns(image=None):
+    images = image_generator()
+    if image is not None:
+        img_path=os.path.join(BASE_DIR, 'data/imgs/')
+        fn = os.path.join(img_path, image)
+        im = load_images([fn])[0].astype('int32')
+        images = itertools.chain([(fn, im)], images)
 
-    return rtn
+    for fn, im in images:
+        imgs = [im]
+        titles = ['Original']
+        print('computing yen threshold')
+        yen = threshold_yen(im)
+        channels = threshold_by_channel(im ,yen)
+        titles.append('Yen Thresholded')
+        imgs.append(channels[0])
+
+        for cp in [175, 200, 225, 250]:
+            titles.append('Cutpoint %d' % cp)
+            print('cutting')
+            rtn = np.zeros(im.shape)
+            rtn[np.average(im, axis=2, weights=[.7, .1, .2]) > cp,0] = 255
+            for i in xrange(1,3):
+                rtn[:, :, i] = rtn[:, :, 0]
+
+            imgs.append(rtn)
+
+        print('plotting')
+        plot_images(
+            imgs,
+            titles=titles,
+            suptitle=fn.split('/')[-1]
+        )
 
 
-def extract_largest_region(mask):
-    rtn = np.copy(mask)
-    regions, n_labels = ndimage.label(mask)
-    sizes = ndimage.sum(mask, regions, range(n_labels+1))
-    max_size = sorted(sizes)[-2]
-    mask_on_size = sizes < max_size
-
-    remove = np.ones(regions.shape)
-    for i in range(1, n_labels+1):
-        if sizes[i] >= max_size:
-            remove[regions==i] = 0
-
-    rtn[remove==1] = 0
-    return rtn
-
-
-def minimum_bounding_rectangle(verts):
-    pi2 = np.pi/2.
-
-    # compute rotation angles
-    verts = np.array(verts)
-    angles = np.empty(verts.shape[0])
-    angles = np.arctan2(verts[:, 1], verts[:, 0])
-
-    # compute rotations
-    rotations = np.vstack([
-        np.cos(angles),
-        np.cos(angles-pi2),
-        np.cos(angles+pi2),
-        np.cos(angles)
-    ]).T
-    rotations = rotations.reshape((-1, 2, 2))
-
-    #apply rotations to the verts
-    rot_points = np.dot(rotations, verts.T)
-
-    # find the bounding points
-    min_x = np.nanmin(rot_points[:, 0], axis=1)
-    max_x = np.nanmax(rot_points[:, 0], axis=1)
-    min_y = np.nanmin(rot_points[:, 1], axis=1)
-    max_y = np.nanmax(rot_points[:, 1], axis=1)
-
-    # find the smallest area
-    areas = (max_x - min_x) * (max_y - min_y)
-    best_idx = np.argmin(areas)
-
-    # extract the best points
-    x1 = max_x[best_idx]
-    x2 = min_x[best_idx]
-    y1 = max_y[best_idx]
-    y2 = min_y[best_idx]
-    r = rotations[best_idx]
-
-    rtn = np.zeros((4,2))
-    rtn[0,:] = np.dot([x1, y2], r)
-    rtn[1,:] = np.dot([x2, y2], r)
-    rtn[2,:] = np.dot([x2, y1], r)
-    rtn[3,:] = np.dot([x1, y1], r)
-
-    return rtn
-
-def extract_variants(binary_image, rgb_image):
+def generate_masks(binary_image, rgb_image):
 
     bin = binary_image > 0
-    s = 1 + 10000 * (bin.sum()/bin.size) ** 1.4
-    s = max(5, 3 * np.log(s))
-    structure = np.ones((s, s))
+    structure = calculate_binary_opening_structure(bin)
     bin = binary_opening(bin, structure=structure)
     rem = np.zeros(binary_image.shape + (3,))
     rem[bin > 0, :] = 255
 
     print('dilation')
-    structure = np.ones((5, 5))
+    strutcure = np.ones((5, 5))
     dilated = np.zeros(binary_image.shape + (3,))
     dilated[binary_dilation(rem[:,:,0], iterations=40) > 0, : ] = 255
 
@@ -322,7 +414,7 @@ def extract_variants(binary_image, rgb_image):
     mask1[dilated > 0] = rgb_image[dilated > 0]
 
     print('largest regions')
-    largest = extract_largest_region(dilated[:,:,0])
+    largest = mask_largest_regions(dilated[:,:,0])
     mask2 = np.zeros(rgb_image.shape)
     for i in xrange(3):
         mask2[largest > 0,i] = rgb_image[largest > 0,i]
@@ -335,7 +427,7 @@ def extract_variants(binary_image, rgb_image):
 
     print('minimal bounding rectangle')
     verts =  convex_hull_mask(largest>0, mask=False)
-    verts = minimum_bounding_rectangle(verts)
+    verts, _, _ = minimum_bounding_rectangle(verts)
     verts = [(verts[i,0], verts[i,1]) for i in range(4)]
     hull = mask_polygon(verts, binary_image.shape)
     mask4 = np.zeros(rgb_image.shape)
@@ -345,11 +437,7 @@ def extract_variants(binary_image, rgb_image):
     return [rem, dilated, mask1, mask2, mask3, mask4]
 
 
-from skimage.feature import canny
-from skimage.transform import hough_ellipse
-from skimage.draw import ellipse_perimeter
-
-def threshold_and_filter(size=15):
+def threshold_and_generate_objects(size=15):
     images = image_generator()
 
     for fn, im in images:
@@ -361,9 +449,107 @@ def threshold_and_filter(size=15):
         imgs = []
         chan = channels[0]
         imgs.append(chan)
-        imgs.extend(extract_variants(chan[:,:,0], im))
+        imgs.extend(generate_masks(chan[:,:,0], im))
         print('plotting')
         plot_images(
             [imgs[0],imgs[-1]],
             suptitle=fn.split('/')[-1]
+        )
+
+
+
+def inspect_local_binary_patterns(file=None, dilation_iterations=40, num_regions=5, radius=2):
+    from skimage.transform import warp
+    from skimage.transform import SimilarityTransform
+    images = image_generator()
+    images = add_image_to_image_generator(images, file)
+    n_points = 8*radius
+
+    for fn, im in images:
+        image_arrays = []
+        histograms = []
+        image_titles = []
+        hist_titles = []
+
+        image_arrays.append(im)
+        image_titles.append('original image')
+
+        yen = threshold_yen(im)
+        yen_channels = threshold_by_channel(im, yen)
+        image_titles.append('Yen Thresholded Channel 1')
+        image_arrays.append(yen_channels[0])
+
+        binary_image = yen_channels[0][:,:,0] > 0
+        structure = calculate_binary_opening_structure(binary_image)
+        binary_image = binary_opening(binary_image, structure=structure)
+        binary_image = binary_dilation(binary_image, iterations=dilation_iterations)
+        regions, labels = extract_largest_regions(binary_image, num_regions=num_regions)
+
+        for label in labels:
+            print('Cropping region %d' % label)
+            region = np.zeros(yen_channels[0].shape[:-1])
+            region[regions == label] = yen_channels[0][regions==label,0]
+            image_titles.append('Region %d' % label)
+            image_arrays.append(binary2gray(region))
+
+            # convex hull
+            verts = convex_hull_mask(region>0, mask=False)
+            verts, rot, angle  = minimum_bounding_rectangle(verts)
+            verts = np.dot(verts, rot).astype(int)
+            inv_rot = np.linalg.inv(rot)
+
+            # translation
+            xtrans = np.abs(min(0,np.min(verts[:,0])))
+            xtrans = xtrans if xtrans > 0 else im.shape[0] - max(np.max(verts[:,0]),im.shape[0])
+            ytrans = np.abs(min(0,np.min(verts[:,1])))
+            ytrans = ytrans if ytrans > 0 else im.shape[1] - max(np.max(verts[:,1]),im.shape[1])
+            translation = -1*np.dot(np.array([xtrans, ytrans]), rot.T).astype(int)
+
+            # crop window
+            xmax = np.max(verts[:,0]) + xtrans
+            xmin = np.min(verts[:,0]) + xtrans
+            ymax = np.max(verts[:,1]) + ytrans
+            ymin = np.min(verts[:,1]) + ytrans
+
+            # transform matrix
+            transform = np.eye(3)
+            transform[0:2,0:2] = rot.T
+            transform[0:2, 2] = [translation[1], translation[0]]
+
+            transform = SimilarityTransform(matrix=transform)
+            crop = np.zeros(im.shape[:-1])
+            crop[:,:] = im[:,:,0] # binary_image * 255
+            crop = warp(crop, transform)
+            crop = crop[xmin:xmax, ymin:ymax]
+            region = warp(region, transform)
+            region = region[xmin:xmax, ymin:ymax]
+
+            rgb_image = np.zeros((xmax-xmin, ymax-ymin, 3), dtype='uint8')
+            for i in range(3):
+                chan = np.zeros(im.shape[:-1])
+                chan[:,:] = im[:,:,0] # binary_image * 255
+                chan = warp(chan, transform)
+                chan = chan[xmin:xmax, ymin:ymax]
+                rgb_image[:,:,i] = chan
+
+            #image_titles.append('Crop %d' % label)
+            #image_arrays.append(rgb_image)
+
+            # lbp features at a set of points
+            lbp = skimage.feature.local_binary_pattern(crop, n_points, radius, method='uniform')
+            lbp = np.ma.compressed(lbp[region > 0])
+            if lbp.size == 0:
+                print('lbp is zero... continuing')
+                continue
+            hist, bins = np.histogram(lbp, bins=20, density=True)
+            hist_titles.append('LBP Histogram for Region %d' % label)
+            histograms.append((hist, bins))
+
+        plot_images_and_histograms(
+            image_arrays=image_arrays,
+            histograms=histograms,
+            image_titles=image_titles,
+            hist_titles=hist_titles,
+            image_suptitle=fn.split('/')[-1],
+            hist_suptitle=fn.split('/')[-1]
         )
--- noaa/utils.py
+++ noaa/utils.py
@@ -42,23 +42,26 @@ def load_images(image_list,
                 source_path=os.path.join(BASE_DIR, 'data/imgs/'),
                 use_path=False):
 
+    is_list = type(image_list) is list
+    image_list = image_list if is_list else [image_list]
+
     images = []
     for image in image_list:
         file = os.path.join(source_path, image) if use_path else image
         with open(file, 'rb') as f:
             images.append(np.asarray(Image.open(f)))
 
-    return images
+    return images if is_list else images[0]
 
 
 ###########################################################################
 ## plot
 
-def plot_images(image_arrays, dims=None, image_names=None, suptitle=None):
+def plot_images(image_arrays, dims=None, titles=None, suptitle=None):
     if not isinstance(image_arrays, list):
         image_arrays=[image_arrays]
-        if image_names is not None:
-            image_names=[image_names]
+        if titles is not None:
+            titles=[titles]
 
     n = len(image_arrays)
     if dims is None:
@@ -75,21 +78,108 @@ def plot_images(image_arrays, dims=None, image_names=None, suptitle=None):
         sharey=True,
         subplot_kw={'adjustable':'box-forced'}
     )
+    if suptitle is not None:
+        fig.suptitle(suptitle, fontsize=14)
+
     if width > 1 or height > 1:
         axes = axes.ravel()
     else:
         axes = [axes]
+
     for i, ax in enumerate(axes):
+        print(image_arrays[i].shape)
         if i >= n:
-            break
-        ax.imshow(image_arrays[i].astype('uint8'))
-        if image_names is not None:
-            ax.set_title(image_names[i])
+            ax.imshow(np.zeros(image_arrays[0].shape).astype('uint8'))
+        else:
+            ax.imshow(image_arrays[i].astype('uint8'))
+            if titles is not None: ax.set_title(titles[i])
+
         ax.axis('off')
 
-    #fig.tight_layout(pad=0.0, w_pad=0.0, h_pad=0.0)
-    if suptitle is not None:
-        fig.suptitle(suptitle, fontsize=14)
+    plt.show()
+    plt.clf()
+
+
+def plot_images_and_histograms(image_arrays, histograms,
+                               image_titles=None, hist_titles=None,
+                               image_suptitle=None, hist_suptitle=None):
+
+    # plot images
+    if not isinstance(image_arrays, list):
+        image_arrays=[image_arrays]
+        if image_titles is not None:
+            image_titles=[image_titles]
+
+    n_images = len(image_arrays)
+    width = int(round(np.sqrt(n_images), 0))
+    height = int(np.ceil(n_images/width))
+
+    fig, axes = plt.subplots(
+        nrows=height,
+        ncols=width,
+        figsize=(16 * height / (width + height), 16 * width / (width + height)),
+        sharex=True,
+        sharey=True,
+        subplot_kw={'adjustable':'box-forced'}
+    )
+    if image_suptitle is not None:
+        fig.suptitle(image_suptitle, fontsize=14)
+
+    if width > 1 or height > 1:
+        axes = axes.ravel()
+    else:
+        axes = [axes]
+
+    for i, ax in enumerate(axes):
+        if i >= n_images:
+            ax.imshow(np.zeros(image_arrays[0].shape).astype('uint8'))
+        else:
+            ax.imshow(image_arrays[i].astype('uint8'))
+            if image_titles is not None: ax.set_title(image_titles[i])
+
+        ax.axis('off')
+
+    if histograms is None:
+        plt.show()
+
+    # plot histograms
+    plt.figure()
+    if not isinstance(histograms, list):
+        histograms=[histograms]
+        if hist_titles is not None:
+            hist_titles=[hist_titles]
+
+    n_hists = len(histograms)
+    width = int(round(np.sqrt(n_hists), 0))
+    height = int(np.ceil(n_hists/width))
+
+    fig, axes = plt.subplots(
+        nrows=height,
+        ncols=width,
+        figsize=(16 * height / (width + height), 16 * width / (width + height)),
+        sharex=True,
+        sharey=True,
+        subplot_kw={'adjustable':'box-forced'}
+    )
+    if image_suptitle is not None:
+        fig.suptitle(image_suptitle, fontsize=14)
+
+    if width > 1 or height > 1:
+        axes = axes.ravel()
+    else:
+        axes = [axes]
+
+    for i, ax in enumerate(axes):
+        if i >= n_hists:
+            break
+        else:
+            hist, bins = histograms[i]
+            width = 0.7 * (bins[1] - bins[0])
+            center = (bins[:-1] + bins[1:]) / 2
+            ax.bar(center, hist, align='center', width=width)
+            if hist_titles is not None: ax.set_title(hist_titles[i])
+
+
 
     plt.show()
     plt.clf()
